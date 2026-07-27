25334bb98770be86b8fff70a16bbe36cd9bf2cdb
EsoElzantot <eslamelzantot@gmail.com>
2026-07-28 01:32:32 +0300

    create order class and its orderItem & invoice classes

---
+++ S-01/Shipping-System/order.class.dart
@@ -0,0 +1,34 @@
+import 'payment-processor.interface.dart';
+import 'product.class.dart';
+
+class Order {
+  List<OrderItem> items = [];
+  Invoice? invoice;
+
+  void addItem(Product product, int quantity) {
+    items.add(OrderItem(product, quantity));
+  }
+
+  void checkout(IPaymentProcessor payment) {
+    double total = 0.0;
+    for (var item in items) {
+      total += item.product.calcFinalPrice() * item.quantity;
+    }
+    invoice = Invoice(total, "USD");
+    payment.processPayment(total);
+  }
+}
+
+class Invoice {
+  double amount = 0.0;
+  String currency = "USD";
+
+  Invoice(this.amount, this.currency);
+}
+
+class OrderItem {
+  Product product;
+  int quantity;
+
+  OrderItem(this.product, this.quantity);
+}
